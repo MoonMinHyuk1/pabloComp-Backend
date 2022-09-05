1fb5aef1f4f9ca0649485e2747741ee3cd0545ef
Jeonghoo An <austinan@naver.com>
2022-09-05 22:55:41 +0900

    init

---
+++ src/main/java/com/pablocomp/pablocompbackend/PabloCompBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pablocomp.pablocompbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PabloCompBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PabloCompBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pablocomp/pablocompbackend/PabloCompBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablocomp.pablocompbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloCompBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

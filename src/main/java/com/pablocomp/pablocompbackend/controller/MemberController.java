9b349a8741033af95d0e090c8ca6bf24c24ee6eb
MoonMinHyuk1 <moon981018@gmail.com>
2022-09-14 02:39:50 +0900

    Feat : #1 시리얼넘버, 임시비번 기능추가

---
+++ src/main/java/com/pablocomp/pablocompbackend/controller/MemberController.java
@@ -0,0 +1,59 @@
+package com.pablocomp.pablocompbackend.controller;
+
+import com.pablocomp.pablocompbackend.domain.Member;
+import com.pablocomp.pablocompbackend.domain.OrderStatus;
+import com.pablocomp.pablocompbackend.repository.MemberRepository;
+import com.pablocomp.pablocompbackend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    @PostConstruct
+    public void initPost() {
+        for(int i = 1; i <= 10; i++) {
+            Member member = new Member(OrderStatus.ORDER, "123456abc" + i, "abc" + i);
+            memberRepository.save(member);
+        }
+    }
+
+    @GetMapping("/init")
+    @ResponseBody
+    public List<Member> initGet() {
+        List<Member> memberList = memberRepository.findAll();
+
+        return memberList;
+    }
+
+    @PostMapping("/member/serialNum")
+    @ResponseBody
+    public Member findMemberBySerialNumPost(@RequestParam("serialNum") String serialNum) {
+        Optional<Member> findMember = memberRepository.findBySerialNum(serialNum, OrderStatus.ORDER);
+
+        return findMember.get();
+    }
+
+    @PostMapping("/member/tempPw")
+    @ResponseBody
+    public String findMemberByTempPwPost(@RequestParam("serialNum") String serialNum, @RequestParam("tempPw") String tempPw) {
+        Optional<Member> findMember = memberRepository.findBySerialNumAndTempPw(serialNum, tempPw);
+
+        if(findMember.isEmpty()) {
+            return "NO";
+        } else {
+            Member member = findMember.get();
+            memberService.updateOrderStatus(member);
+
+            return "YES";
+        }
+    }
+}
